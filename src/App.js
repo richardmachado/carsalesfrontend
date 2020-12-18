c3156c66d16c52285958e600c26ef44f7d85e273
Richard Machado <mmachado.richard@gmail.com>
2020-12-17 17:20:01 -0800

    lowecase maintenance.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import Login from "./Login/Login";
 import Home from "./components/Home/Home";
 import Inventory from "./components/Inventory/Inventory";
-import Maintenance from "./components/maintenance/Maintenance";
+import Maintenance from "./components/maintenance/maintenance";
 import VinSearch from "./components/VinSearch/VinSearch";
 import AddVehicle from './components/AddVehicle/AddVehicle';
 import Financing from "./components/Financing/Financing"
