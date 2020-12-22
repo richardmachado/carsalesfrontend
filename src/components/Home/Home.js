03261428c2511913b217b00410d59931b3a77896
Richard Machado <mmachado.richard@gmail.com>
2020-12-21 16:18:22 -0800

    starting a different branch for a different project

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 // import dealer from "../pictures/car-dealer.jpg"
-
-
+// import Image from 'react-bootstrap/Image'
+import dog from './dog.jpg'
 import {
-    // Photo,
-    Usedcars,
-    // Usedcarsborder
+    Photo,
+    MainText,
+    BelowMainText,
+    Button
 } from './styles';
 
+import {Link} from 'react-router-dom'
 export class Home extends Component {
     render() {
         return (
             <div>
-                <Usedcars>Hundred of cars for Sale</Usedcars>
+                <Photo src={dog} fluid alt="dog"/>
+                <MainText>Ultimate Outdoor Experience</MainText>
+                <BelowMainText>Find portable pet products right at your fingertips. No more endless searching!</BelowMainText>
+                <Link to="/products">
+                <Button>Shop now!</Button>
+                </Link>
+                <h1>Today's Deals</h1>
             </div>
         )
     }
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const Photo = styled.img`
-width:80%;
-height: 10%;
+position:relative;
+width:100%;
+height:100%;
+
 `
 
 export const Usedcarsborder = styled.div`
@@ -10,11 +12,118 @@ background-color:#dee3d1;
 padding-bottom:2rem;
 margin-top:2rem;
 `
-export const Usedcars = styled.h1`
-font-family: CarMaxSharpSansDisp-Bold,Frutiger,‘Frutiger Linotype’,Univers,Calibri,‘Gill Sans’,‘Gill Sans MT’,‘Myriad Pro’,Myriad,‘DejaVu Sans Condensed’,‘Liberation Sans’,‘Nimbus Sans L’,Tahoma,Geneva,‘Helvetica Neue’,Helvetica,Arial,sans-serif;
+export const MainText = styled.h1`
+position: absolute; 
+top: 300px; 
+left: 0; 
+width: 100%; 
+color:white;
+font-size:100px;
+@media (max-width: 1282px) {
+   font-size:80px;
+  }
+  @media (max-width: 1024px) {
+    font-size:50px;
+  }  
+  @media (max-width: 800px) {
+    font-size:40px;
+    top:200px
+  }  
+  @media (max-width: 512px) {
+    font-size:30px;
+    top:150px
+  }  
+  @media (max-width: 432px) {
+    font-size:20px;
+    top:140px
+  } 
+
+  @media (max-width: 362px) {
+    font-size:15px;
+    top:140px
+  } 
+`
+
+export const BelowMainText = styled.p`
+position: absolute; 
+top: 400px; 
+left: 0; 
+width: 100%; 
+color:white;
+font-size:50px;
+
+@media (max-width: 1282px) {
+    font-size:30px;
+  }
+  @media (max-width: 1082px) {
+    font-size:20px;
+  }
+
+  @media (max-width: 800px) {
+    top: 250px; 
+  }  
+  @media (max-width: 512px) {
+    top:200px
+  }
+  @media (max-width: 432px) {
+    top:180px
+  }
+  @media (max-width: 362px) {
+    font-size:15px;
+    top:160px
+  }    
+`
+
+export const Button = styled.button`
+background-color: #3fc1c9;
+border-color: #3fc1c9;
+color: #fff;
+transition: all 0.3s ease;
+font-family: "DIN Next",sans-serif;
 font-weight: 700;
-font-size: 40px;
-line-height: 1.25;
-letter-spacing: .25px;
-color: #053361;
+font-style: normal;
+text-transform: initial;
+letter-spacing: 0.5px;
+border: 1px solid #f2f2f2;
+border-radius: 3px;
+padding: 16px 24px;
+line-height: 1.5;
+display: inline-block;
+text-align: center;
+font-size: 17px;
+
+position: absolute; 
+top: 750px;
+right:50%; 
+
+@media (max-width: 1282px) {
+    position: absolute; 
+    top: 500px;
+    right:50%; 
+  }
+
+  @media (max-width: 1022px) {
+    position: absolute; 
+    top: 500px;
+    right:50%; 
+  }
+  @media (max-width: 800px) {
+    top: 350px; 
+    right:40%
+  }  
+  @media (max-width: 512px) {
+   top:300px;
+   right:40%
+  }  
+  @media (max-width: 432px) {
+
+    top:260px;
+    right:40%
+  } 
+  @media (max-width: 362px) {
+
+    top:220px;
+    right:30%;
+     padding: 8px 18px;
+  } 
 `
